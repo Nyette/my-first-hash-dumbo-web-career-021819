f0bf8d60ab87443feceb2d068427a21eb39be04d
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-01 07:50:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = {"my_name" => "Nette", "my_race" => "vampire", "my_size" => "tiny"}
+  my_hash = {"race" => "vampire", "age" => 500}
 end
 
 
@@ -13,29 +13,24 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+    "whale bone corsets" => 5, 
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
+  }
   # add 2 muskets to the shipping_manifest hash below
-  
-
-
+  shipping_manifest["muskets"] = 2
   # add 4 gun powder to the shipping_manifest hash below
-
-
-
+  shipping_manifest["gun powder"] = 4
   # return the shipping_manifest hash below
-
+  shipping_manifest
 end
